1caca5b63efd1232af0c52292a9a19bf3c6584da
김수연 <suyeun5678@naver.com>
2022-06-22 16:23:28 +0900

    phantom1

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Connect2Phantom from './components/Connect2Phantom';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Solana Examples</h1>
+        <hr className="fullWidth" />
+
+        <p>Hello there</p>
+        <Connect2Phantom/>
+   
+      </header>
+    </div>
+  );
+}
+
+export default App;
